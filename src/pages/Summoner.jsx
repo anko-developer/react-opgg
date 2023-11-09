4ca455cb4a6f036ae48d293eb530dd488f60ec9c
김명욱 <anko@gsitm.com>
2023-11-09 10:57:53 +0900

    api 연동

--- src/pages/Summoner.jsx
+++ src/pages/Summoner.jsx
@@ -1,34 +1,28 @@
 import React from "react";
-import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { useRoitApi } from "../context/RoitApiContext";
+import { useParams } from "react-router-dom";
 
 export default function Summoner() {
-  const client = axios.create({
-    baseURL: "https://kr.api.riotgames.com/lol/",
-    params: { api_key: process.env.REACT_APP_RIOT_API_KEY },
-  });
-
-  const info = async () => {
-    return client
-      .get("summoner/v4/summoners/by-name/hide on bush")
-      .then((res) => res.data)
-      .catch((error) => console.log(error));
-  };
-
+  const { roit } = useRoitApi();
+  const { summonerName } = useParams();
   const {
     isLoading,
     error,
     data: summonerInfo,
   } = useQuery({
-    queryKey: ["summoner"],
-    queryFn: () => info(),
+    queryKey: ["summoner", summonerName],
+    queryFn: () => roit.searchSummoner(summonerName),
   });
+
   return (
     <div>
       {isLoading && <p>Loading...</p>}
       {error && <p>Error...</p>}
       {summonerInfo && (
         <p>
+          닉네임: {summonerName}
+          <br />
           id: {summonerInfo.id}
           name: {summonerInfo.name}
           level: {summonerInfo.summonerLevel}
